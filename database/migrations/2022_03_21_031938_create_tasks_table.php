7e075b9eb50678adcc859a5a8bf73a3962d1675d
takeshi_honda <honda0924@hotmail.co.jp>
2022-03-21 13:55:44 +0900

    feat: create migration and seeder

--- database/migrations/2022_03_21_031938_create_tasks_table.php
+++ database/migrations/2022_03_21_031938_create_tasks_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('title', 100);
             $table->string('content', 100);
-            $table->string('person_in_charge)', 100);
+            $table->string('person_in_charge', 100);
             $table->timestamps();
         });
     }
